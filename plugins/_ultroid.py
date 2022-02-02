0ba73463774c4ee328a44f79486986db402c7ae6
Shoto <76978884+INDOHACKER-XODE@users.noreply.github.com>
2022-02-02 20:53:02 +0700

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -14,10 +14,10 @@ from telethon.errors import (
 from . import LOG_CHANNEL, LOGS, Button, asst, eor, get_string, ultroid_cmd
 
 REPOMSG = """
-• **ULTROID USERBOT** •\n
+• **Yui USERBOT** •\n
 • Repo - [Click Here](https://github.com/TeamUltroid/Ultroid)
 • Addons - [Click Here](https://github.com/TeamUltroid/UltroidAddons)
-• Support - @UltroidSupport
+• Support - @YuiHirasawauserbot
 """
 
 RP_BUTTONS = [
